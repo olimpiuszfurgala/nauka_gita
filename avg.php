ddc2e834247767120f2620fcb7b52fb3f96b557e
olimpiuszfurgala <olimpiuszfurgala@gmail.com>
2026-05-17 18:10:01 +0200

    Inne wagi 7=>8, 3=>2

--- avg.php
+++ avg.php
@@ -1,2 +1,3 @@
 <?php function srednia($a,$b){ return ($a+$b)/2; } ?>
 <?php function srednia_wazona($a,$b){ return (($a*7)+($b*3))/10; } ?>
+<?php function srednia_wazona($a,$b){ return (($a*8)+($b*2))/10; } ?>
